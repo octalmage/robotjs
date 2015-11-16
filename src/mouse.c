406acfb5e730f2b6777ba98f585a099b1e986bdf
Nathan Ostgard <noonat@phuce.com>
2015-11-16 09:00:09 -0800

    Added dragMouse function
    Fixes #127

--- src/mouse.c
+++ src/mouse.c
@@ -18,6 +18,44 @@
 	#define M_SQRT2 1.4142135623730950488016887 /* Fix for MSVC. */
 #endif
 
+/* Some convenience macros for converting our enums to the system API types. */
+#if defined(IS_MACOSX)
+
+#define MMMouseToCGEventType(down, button) \
+	(down ? MMMouseDownToCGEventType(button) : MMMouseUpToCGEventType(button))
+
+#define MMMouseDownToCGEventType(button) \
+	((button) == (LEFT_BUTTON) ? kCGEventLeftMouseDown \
+	                       : ((button) == RIGHT_BUTTON ? kCGEventRightMouseDown \
+	                                                   : kCGEventOtherMouseDown))
+
+#define MMMouseUpToCGEventType(button) \
+	((button) == LEFT_BUTTON ? kCGEventLeftMouseUp \
+	                         : ((button) == RIGHT_BUTTON ? kCGEventRightMouseUp \
+	                                                     : kCGEventOtherMouseUp))
+
+#define MMMouseDragToCGEventType(button) \
+	((button) == LEFT_BUTTON ? kCGEventLeftMouseDragged \
+	                         : ((button) == RIGHT_BUTTON ? kCGEventRightMouseDragged \
+	                                                     : kCGEventOtherMouseDragged))
+
+#elif defined(IS_WINDOWS)
+
+#define MMMouseToMEventF(down, button) \
+	(down ? MMMouseDownToMEventF(button) : MMMouseUpToMEventF(button))
+
+#define MMMouseUpToMEventF(button) \
+	((button) == LEFT_BUTTON ? MOUSEEVENTF_LEFTUP \
+	                         : ((button) == RIGHT_BUTTON ? MOUSEEVENTF_RIGHTUP \
+	                                                     : MOUSEEVENTF_MIDDLEUP))
+
+#define MMMouseDownToMEventF(button) \
+	((button) == LEFT_BUTTON ? MOUSEEVENTF_LEFTDOWN \
+	                         : ((button) == RIGHT_BUTTON ? MOUSEEVENTF_RIGHTDOWN \
+	                                                     : MOUSEEVENTF_MIDDLEDOWN))
+
+#endif
+
 /**
  * Move the mouse to a specific point.
  * @param point The coordinates to move the mouse to (x, y).
@@ -41,6 +79,20 @@ void moveMouse(MMPoint point)
 	point.y = MOUSE_COORD_TO_ABS(point.y, GetSystemMetrics(SM_CYSCREEN));
 	mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE,
 	            (DWORD)point.x, (DWORD)point.y, 0, 0);
+#endif
+}
+
+void dragMouse(MMPoint point, const MMMouseButton button)
+{
+#if defined(IS_MACOSX)
+	const CGEventType dragType = MMMouseDragToCGEventType(button);
+	const CGEventRef drag = CGEventCreateMouseEvent(NULL, dragType,
+	                                                CGPointFromMMPoint(point),
+	                                                (CGMouseButton)button);
+	CGEventPost(kCGSessionEventTap, drag);
+	CFRelease(drag);
+#else
+	moveMouse(point);
 #endif
 }
 
@@ -70,39 +122,6 @@ MMPoint getMousePos()
 	return MMPointFromPOINT(point);
 #endif
 }
-
-/* Some convenience macros for converting our enums to the system API types. */
-#if defined(IS_MACOSX)
-
-#define MMMouseToCGEventType(down, button) \
-	(down ? MMMouseDownToCGEventType(button) : MMMouseUpToCGEventType(button))
-
-#define MMMouseDownToCGEventType(button) \
-	((button) == (LEFT_BUTTON) ? kCGEventLeftMouseDown \
-	                       : ((button) == RIGHT_BUTTON ? kCGEventRightMouseDown \
-	                                                   : kCGEventOtherMouseDown))
-
-#define MMMouseUpToCGEventType(button) \
-	((button) == LEFT_BUTTON ? kCGEventLeftMouseUp \
-	                         : ((button) == RIGHT_BUTTON ? kCGEventRightMouseUp \
-	                                                     : kCGEventOtherMouseUp))
-
-#elif defined(IS_WINDOWS)
-
-#define MMMouseToMEventF(down, button) \
-	(down ? MMMouseDownToMEventF(button) : MMMouseUpToMEventF(button))
-
-#define MMMouseUpToMEventF(button) \
-	((button) == LEFT_BUTTON ? MOUSEEVENTF_LEFTUP \
-	                         : ((button) == RIGHT_BUTTON ? MOUSEEVENTF_RIGHTUP \
-	                                                     : MOUSEEVENTF_MIDDLEUP))
-
-#define MMMouseDownToMEventF(button) \
-	((button) == LEFT_BUTTON ? MOUSEEVENTF_LEFTDOWN \
-	                         : ((button) == RIGHT_BUTTON ? MOUSEEVENTF_RIGHTDOWN \
-	                                                     : MOUSEEVENTF_MIDDLEDOWN))
-
-#endif
 
 /**
  * Press down a button, or release it.
