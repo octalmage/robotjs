8e248027131e0577182ac759c806837856be973b
Jason Stallings <jason@stallin.gs>
2016-05-24 22:47:35 -0500

    Add numpad tests.

--- test/keyboard.js
+++ test/keyboard.js
@@ -21,13 +21,26 @@ test('Tap a key.', function(t)
 });
 
 // This test won't fail if there's an issue, but it will help you identify an issue if ran locally.
-test('Tap all keys', function(t)
+test('Tap all keys.', function(t)
 {
 	var chars = 'abcdefghijklmnopqrstuvwxyz1234567890,./;\'[]\\'.split('');
 
 	for (var x in chars)
 	{
 		t.ok(robot.keyTap(chars[x]), 'tap ' + chars[x] + '.');
+	}
+
+	t.end();
+});
+
+// This test won't fail if there's an issue, but it will help you identify an issue if ran locally.
+test('Tap all numpad keys.', function(t)
+{
+	var nums = '0123456789'.split('');
+
+	for (var x in nums)
+	{
+		t.ok(robot.keyTap('numpad_' + nums[x]), 'tap ' + 'numpad_' + nums[x] + '.');
 	}
 
 	t.end();
