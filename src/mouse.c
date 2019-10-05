a348c0e36f84e934acccd17f2a9151cfd928246e
Gerald <gera2ld@163.com>
2019-10-06 01:18:07 +0800

    fix: improve y scrolling on Windows by send y-scrolling event first

--- src/mouse.c
+++ src/mouse.c
@@ -300,22 +300,23 @@ void scrollMouse(int x, int y)
 
 #elif defined(IS_WINDOWS)
 
+	// Must send y first, otherwise we get stuck when scrolling on y axis
 	mouseScrollInputs[0].type = INPUT_MOUSE;
 	mouseScrollInputs[0].mi.dx = 0;
 	mouseScrollInputs[0].mi.dy = 0;
-	mouseScrollInputs[0].mi.dwFlags = MOUSEEVENTF_HWHEEL;
+	mouseScrollInputs[0].mi.dwFlags = MOUSEEVENTF_WHEEL;
 	mouseScrollInputs[0].mi.time = 0;
 	mouseScrollInputs[0].mi.dwExtraInfo = 0;
-	// Flip x to match other platforms.
-	mouseScrollInputs[0].mi.mouseData = -x;
+	mouseScrollInputs[0].mi.mouseData = y;
 
 	mouseScrollInputs[1].type = INPUT_MOUSE;
 	mouseScrollInputs[1].mi.dx = 0;
 	mouseScrollInputs[1].mi.dy = 0;
-	mouseScrollInputs[1].mi.dwFlags = MOUSEEVENTF_WHEEL;
+	mouseScrollInputs[1].mi.dwFlags = MOUSEEVENTF_HWHEEL;
 	mouseScrollInputs[1].mi.time = 0;
 	mouseScrollInputs[1].mi.dwExtraInfo = 0;
-	mouseScrollInputs[1].mi.mouseData = y;
+	// Flip x to match other platforms.
+	mouseScrollInputs[1].mi.mouseData = -x;
 
 	SendInput(2, mouseScrollInputs, sizeof(INPUT));
 #endif
