502251bc5378afe64920d5224d4a69ff1af69e34
Ray Foss <ray@vblip.com>
2020-03-31 12:37:00 -0500

    hardened-screen-test

--- test/integration/screen.js
+++ test/integration/screen.js
@@ -17,9 +17,18 @@ describe('Integration/Screen', () => {
 		target = null;
 	});
 
-	it('reads a pixel color', () => {
-		var color_1 = elements.color_1;
-		const color = robot.getPixelColor(color_1.x, color_1.y);
-		expect(color).toEqual('c0ff33');
+	it('reads a pixel color', (done) => {
+		const maxDelay = 1000
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = maxDelay + 1000
+		const expected = 'c0ff33'
+		const color_1 = elements.color_1;
+		const sleepTime = robot.getPixelColor(color_1.x, color_1.y) === expected ? 0
+			: maxDelay
+
+		setTimeout(() => {
+			const color = robot.getPixelColor(color_1.x, color_1.y);
+			expect(color).toEqual(expected);
+			done()
+		}, sleepTime)
 	});
 });
