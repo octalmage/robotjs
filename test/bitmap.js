5388935aab960dd572c2eb157446b0fdcee7b95a
Jason Stallings <octalmage@gmail.com>
2026-03-14 09:30:47 -0500

    test: Additional bitmap tests.

--- test/bitmap.js
+++ test/bitmap.js
@@ -1,7 +1,8 @@
-var robot = require('..');
+const robot = require('..');
+const makeBitmap = require('./helpers/images').makeBitmap;
 
 describe('Bitmap', () => {
-  var params = {
+  const params = {
 		'width': 'number',
 		'height': 'number',
 		'byteWidth': 'number',
@@ -11,9 +12,9 @@ describe('Bitmap', () => {
 	};
 
 	it('Get a bitmap and check the parameters.', function() {
-		var img = robot.screen.capture();
+		const img = robot.screen.capture();
 
-		for (var x in params)
+		for (const x in params)
 		{
 			expect(typeof img[x]).toEqual(params[x]);
 		}
@@ -21,30 +22,30 @@ describe('Bitmap', () => {
 
 	it('Get a bitmap of a specific size.', function()
 	{
-		var size = 10;
-		var img = robot.screen.capture(0, 0, size, size);
+		const size = 10;
+		const img = robot.screen.capture(0, 0, size, size);
 
 		// Support for higher density screens.
-		var multi = img.width / size;
-		var size = size * multi;
-		expect(img.height).toEqual(size);
-		expect(img.width).toEqual(size);
+		const multi = img.width / size;
+		const scaledSize = size * multi;
+		expect(img.height).toEqual(scaledSize);
+		expect(img.width).toEqual(scaledSize);
 	});
 
 	it('Get a bitmap and make sure the colorAt works as expected.', function()
 	{
-		var img = robot.screen.capture();
-		var hex = img.colorAt(0, 0);
+		const img = robot.screen.capture();
+		const hex = img.colorAt(0, 0);
 
 		// t.ok(.it(hex), "colorAt returned valid hex.");
 		expect(hex).toMatch(/^[0-9A-F]{6}$/i);
 
-		var screenSize = robot.getScreenSize();
-		var width = screenSize.width;
-		var height = screenSize.height;
+		const screenSize = robot.getScreenSize();
+		let width = screenSize.width;
+		let height = screenSize.height;
 
 		// Support for higher density screens.
-		var multi = img.width / width;
+		const multi = img.width / width;
 		width = width * multi;
 		height = height * multi;
 		expect(() => img.colorAt(0, height)).toThrowError(/are outside the bitmap/);
@@ -52,5 +53,57 @@ describe('Bitmap', () => {
 		expect(() => img.colorAt(width, 0)).toThrowError(/are outside the bitmap/);
 		expect(() => img.colorAt(9999999999999, 0)).toThrowError(/are outside the bitmap/);
 		expect(() => img.colorAt(0, 9999999999999)).toThrowError(/are outside the bitmap/);
+	});
+
+	it('Reads padded rows without crossing into row padding.', function() {
+		const img = makeBitmap([
+			['ff0000', '00ff00', '0000ff'],
+			['ffff00', 'ff00ff', '00ffff']
+		], { byteWidth: 16, fill: 0xEE });
+
+		expect(img.colorAt(0, 0)).toEqual('ff0000');
+		expect(img.colorAt(1, 0)).toEqual('00ff00');
+		expect(img.colorAt(2, 0)).toEqual('0000ff');
+		expect(img.colorAt(0, 1)).toEqual('ffff00');
+		expect(img.colorAt(1, 1)).toEqual('ff00ff');
+		expect(img.colorAt(2, 1)).toEqual('00ffff');
+	});
+
+	it('Finds colors inside sub-rectangles with non-zero origins.', function() {
+		const img = makeBitmap([
+			['111111', '222222', '333333', '444444'],
+			['555555', '666666', '777777', 'ff00ff'],
+			['888888', 'ff00ff', '999999', 'aaaaaa']
+		], { byteWidth: 20, fill: 0xEE });
+
+		expect(img.findColor('ff00ff')).toEqual({ x: 3, y: 1 });
+		expect(img.findColor('ff00ff', { x: 1, y: 2, width: 1, height: 1 })).toEqual({ x: 1, y: 2 });
+		expect(img.findColors('ff00ff', { x: 1, y: 1, width: 3, height: 2 })).toEqual([
+			{ x: 3, y: 1 },
+			{ x: 1, y: 2 }
+		]);
+		expect(img.countColor('ff00ff', { x: 1, y: 1, width: 3, height: 2 })).toEqual(2);
+		expect(img.findColor('abcdef')).toBeNull();
+	});
+
+	it('Finds bitmaps inside padded haystacks and respects search rect origins.', function() {
+		const haystack = makeBitmap([
+			['101010', '101010', '101010', '101010', '101010', '101010'],
+			['101010', 'ff0000', '0000ff', 'ff0000', '0000ff', '101010'],
+			['101010', '00ff00', 'ffff00', '00ff00', 'ffff00', '101010'],
+			['101010', '101010', '101010', '101010', '101010', '101010']
+		], { byteWidth: 28, fill: 0xEE });
+		const needle = makeBitmap([
+			['ff0000', '0000ff'],
+			['00ff00', 'ffff00']
+		], { byteWidth: 12, fill: 0xEE });
+
+		expect(haystack.findImage(needle)).toEqual({ x: 1, y: 1 });
+		expect(haystack.findImages(needle, { x: 1, y: 1, width: 4, height: 2 })).toEqual([
+			{ x: 1, y: 1 },
+			{ x: 3, y: 1 }
+		]);
+		expect(haystack.countImage(needle, { x: 1, y: 1, width: 4, height: 2 })).toEqual(2);
+		expect(haystack.findImage(needle, { x: 0, y: 0, width: 1, height: 4 })).toBeNull();
 	});
 });
--- test/image.js
+++ test/image.js
@@ -2,72 +2,7 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const robot = require('..');
-
-function writeColor(buffer, byteWidth, bytesPerPixel, x, y, hex) {
-	const offset = (y * byteWidth) + (x * bytesPerPixel);
-	const color = parseInt(hex, 16);
-
-	buffer[offset] = color & 0xFF;
-	buffer[offset + 1] = (color >> 8) & 0xFF;
-	buffer[offset + 2] = (color >> 16) & 0xFF;
-	if (bytesPerPixel === 4) {
-		buffer[offset + 3] = 0xFF;
-	}
-}
-
-function makeBitmap(rows, options) {
-	options = options || {};
-
-	const width = rows[0].length;
-	const height = rows.length;
-	const bytesPerPixel = options.bytesPerPixel || 4;
-	const byteWidth = options.byteWidth || (width * bytesPerPixel);
-	const image = Buffer.alloc(byteWidth * height, 0x00);
-
-	rows.forEach(function(row, y) {
-		row.forEach(function(hex, x) {
-			writeColor(image, byteWidth, bytesPerPixel, x, y, hex);
-		});
-	});
-
-	return new robot.Image(width, height, byteWidth, bytesPerPixel * 8, bytesPerPixel, image);
-}
-
-function create24BitBMP(rows) {
-	const width = rows[0].length;
-	const height = rows.length;
-	const rowStride = (width * 3 + 3) & ~3;
-	const imageSize = rowStride * height;
-	const fileSize = 54 + imageSize;
-	const buffer = Buffer.alloc(fileSize, 0x00);
-
-	buffer.writeUInt16LE(0x4D42, 0);
-	buffer.writeUInt32LE(fileSize, 2);
-	buffer.writeUInt32LE(54, 10);
-
-	buffer.writeUInt32LE(40, 14);
-	buffer.writeInt32LE(width, 18);
-	buffer.writeInt32LE(height, 22);
-	buffer.writeUInt16LE(1, 26);
-	buffer.writeUInt16LE(24, 28);
-	buffer.writeUInt32LE(0, 30);
-	buffer.writeUInt32LE(imageSize, 34);
-
-	rows.slice().reverse().forEach(function(row, rowIndex) {
-		const rowOffset = 54 + (rowIndex * rowStride);
-
-		row.forEach(function(hex, x) {
-			const color = parseInt(hex, 16);
-			const pixelOffset = rowOffset + (x * 3);
-
-			buffer[pixelOffset] = color & 0xFF;
-			buffer[pixelOffset + 1] = (color >> 8) & 0xFF;
-			buffer[pixelOffset + 2] = (color >> 16) & 0xFF;
-		});
-	});
-
-	return buffer;
-}
+const { create24BitBMP, makeBitmap } = require('./helpers/images');
 
 describe('Image', function() {
 	it('Loads a BMP file and uses it as a search bitmap.', function() {
